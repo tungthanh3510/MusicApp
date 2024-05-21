104be890442ce38bb10263c3b2a1098f69549d56
tungthanh3510 <114477704+tungthanh3510@users.noreply.github.com>
2024-05-21 18:39:49 +0700

    Update settings.py

--- music/settings.py
+++ music/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import pymysql
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
