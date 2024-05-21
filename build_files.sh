849cf4da6909745e6cf9181b392cea21cb58b601
tungthanh3510 <114477704+tungthanh3510@users.noreply.github.com>
2024-05-21 20:26:12 +0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -7,13 +7,13 @@
 #source .venv/bin/activate
 
 # Upgrade pip
-pip install --upgrade pip
+#pip install --upgrade pip
 
 # Install dependencies
 pip install -r requirements.txt
 
 # Run database migrations
-python manage.py migrate
+#python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
